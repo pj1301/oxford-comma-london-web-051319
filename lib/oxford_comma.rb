bc72b87da695fc0f24fd85dea946e194758563e6
James Hayward <james.hayward6@icloud.com>
2019-04-23 08:48:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,27 +1,4 @@
 def oxford_comma(array)
-  # case array
-  # when array.length == 1
-  #   array[0]
-  # when array.length == 2
-  #   "#{array[0..1].join(" and ")}"
-  # when array.length == 3
-  #   "#{array[0..1].join(", ")} and #{array.last}"
-  # else array.length > 3
-  #   "#{array[0..array.length-1].join(", ")} and #{array.last}"
-  # end
-
-  # if array.length == 1
-  #   array[0]
-  # elsif array.length == 2
-  #   array[1] = "and #{array[1]}"
-  #   "#{array.join(" ")}"
-  # elsif array.length == 3
-  #   array[2] = "and #{array[2]}"
-  #   "#{array.join(", ")}"
-  # elsif array.length > 3
-  #   array[array.length-1] = "and #{array[array.length-1]}"
-  #   "#{array.join(", ")}"
-  # end
 
   if array.length == 1
     array[0]

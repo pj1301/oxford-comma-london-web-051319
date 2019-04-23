2678fca3b11c4ed6531610285cb6586549cb4bdd
James Hayward <james.hayward6@icloud.com>
2019-04-23 08:47:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -10,18 +10,23 @@ def oxford_comma(array)
   #   "#{array[0..array.length-1].join(", ")} and #{array.last}"
   # end
 
+  # if array.length == 1
+  #   array[0]
+  # elsif array.length == 2
+  #   array[1] = "and #{array[1]}"
+  #   "#{array.join(" ")}"
+  # elsif array.length == 3
+  #   array[2] = "and #{array[2]}"
+  #   "#{array.join(", ")}"
+  # elsif array.length > 3
+  #   array[array.length-1] = "and #{array[array.length-1]}"
+  #   "#{array.join(", ")}"
+  # end
+
   if array.length == 1
     array[0]
-  elsif array.length == 2
-    array[1] = "and #{array[1]}"
-    "#{array.join(" ")}"
-  elsif array.length == 3
-    array[2] = "and #{array[2]}"
-    "#{array.join(", ")}"
-  elsif array.length > 3
+  else
     array[array.length-1] = "and #{array[array.length-1]}"
     "#{array.join(", ")}"
   end
-
-
 end

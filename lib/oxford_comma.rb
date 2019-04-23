32de33ff5fcf3da95f748b9b8d25564cadc3f597
James Hayward <james.hayward6@icloud.com>
2019-04-23 08:43:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -16,9 +16,11 @@ def oxford_comma(array)
     array[1] = "and #{array[1]}"
     "#{array.join(", ")}"
   elsif array.length == 3
-    "#{array[0..1].join(", ")} and #{array.last}"
+    array[2] = "and #{array[2]}"
+    "#{array.join(", ")}"
   elsif array.length > 3
-    "#{array[0..array.length-1].join(", ")} and #{array.last}"
+    array[] = "and #{array[1]}"
+    "#{array.join(", ")}"
   end
 
 

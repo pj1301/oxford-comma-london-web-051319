638e645c19d70ae6ce22adafa3655112a1a43949
James Hayward <james.hayward6@icloud.com>
2019-04-23 08:48:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -25,6 +25,9 @@ def oxford_comma(array)
 
   if array.length == 1
     array[0]
+  elsif array.length == 2
+    array[1] = "and #{array[1]}"
+    "#{array.join(" ")}"
   else
     array[array.length-1] = "and #{array[array.length-1]}"
     "#{array.join(", ")}"

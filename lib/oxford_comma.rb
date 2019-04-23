f1b2f7c0c2fef6f0cd041e20017ee16aef2003f0
James Hayward <james.hayward6@icloud.com>
2019-04-23 08:12:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
   case array
   when array.length == 1
-    "#{array[0]}"
+    puts "#{array[0]}"
   when array.length == 2
-    "#{array[0..1].join(" and ")}"
+    puts "#{array[0..1].join(" and ")}"
   when array.length == 3
     "#{array[0..1].join(", ")} and #{array.last}"
   else array.length > 3
